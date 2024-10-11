08ae81436c57ead62ad0d01c323a00405035a063
Ali Hassan Ali <69222334+Ali-Hassan-Ali@users.noreply.github.com>
2024-10-11 11:28:01 +0300

    add sortable to datatable
    add sortable to datatable

--- resources/views/components/dashboard/admin/data-table/script.blade.php
+++ resources/views/components/dashboard/admin/data-table/script.blade.php
@@ -1,81 +1,103 @@
 <script>
 
-    datatable = @json($datatables);
-    myColumns = [];
+    $(function() {
 
-    myColumns.push({data: 'record_select', name: 'record_select', searchable: false, sortable: false, width: '1%'});
-    $.each(datatable.columns, (key, value) => myColumns.push({data: key, name: value}));
-    myColumns.push({data: 'created_at', name: 'created_at', searchable: false});
-    myColumns.push({data: 'actions', name: 'actions', searchable: false, sortable: false, width: '20%'});
-    
-    @php($lang = in_array(app()->getLocale(), ['ar', 'en']) ? app()->getLocale() : 'en')
+        datatable = @json($datatables);
+        myColumns = [];
 
-    let Table = $('#data-table').DataTable({
-        dom: "tiplr",
-        scrollY: '500px',
-        sScrollX: "100%",
-        scrollCollapse: true,
-        serverSide: true,
-        processing: true,
-        language: {url: "{{ asset('admin_assets/datatable-lang/' . $lang . '.json') }}"},
-        ajax: {url: datatable.route},
-        columns: myColumns,
-        drawCallback: function (settings) {
-            $('.record__select').prop('checked', false);
-            $('#record__select-all').prop('checked', false);
-            $('#record-ids').val();
-            $('#bulk-delete').attr('disabled', true);
-        }
-    });
+        myColumns.push({data: 'record_select', name: 'record_select', searchable: false, sortable: false, width: '1%'});
+        $.each(datatable.columns, (key, value) => myColumns.push({data: key, name: value}));
+        myColumns.push({data: 'created_at', name: 'created_at', searchable: false});
+        myColumns.push({data: 'actions', name: 'actions', searchable: false, sortable: false, width: '20%'});
+        
+        @php($lang = in_array(app()->getLocale(), ['ar', 'en']) ? app()->getLocale() : 'en')
 
-    $('#data-table-search').keyup(function () {
-        Table.search(this.value).draw();
-    })
+        var Table = $('#data-table').DataTable({
+            dom: "tiplr",
+            scrollY: '500px',
+            sScrollX: "100%",
+            scrollCollapse: true,
+            serverSide: true,
+            processing: true,
+            language: {url: "{{ asset('admin_assets/datatable-lang/' . $lang . '.json') }}"},
+            ajax: {url: datatable.route},
+            columns: myColumns,
+            createdRow: (row, data, dataIndex) => {$(row).attr('data-id', data.id)},
+            drawCallback: function (settings) {
+                $('.record__select').prop('checked', false);
+                $('#record__select-all').prop('checked', false);
+                $('#record-ids').val();
+                $('#bulk-delete').attr('disabled', true);
+            }
+        });
 
-    if (datatable.checkbox) {
+        $('#data-table-search').keyup(function () {
+            Table.search(this.value).draw();
+        })
 
-        $(document).on('change', '.checkbox', function (e) {
-            e.preventDefault();
+        if (datatable.checkbox) {
 
-            let type   = $(this).data('type');
-            let url    = datatable.checkbox[type];
-            let method = 'post';
-            let id     = $(this).data('id');
+            $(document).on('change', '.checkbox', function (e) {
+                e.preventDefault();
 
-            $.ajax({
-                url: url,
-                data: {id: id},
-                method: method,
-                success: function (response) {
+                let type   = $(this).data('type');
+                let url    = datatable.checkbox[type];
+                let method = 'post';
+                let id     = $(this).data('id');
 
-                    $('.datatable').DataTable().ajax.reload();
+                $.ajax({
+                    url: url,
+                    data: {id: id},
+                    method: method,
+                    success: function (response) {
 
-                    new Noty({
-                        layout: 'topRight',
-                        type: 'alert',
-                        text: response,
-                        killer: true,
-                        timeout: 2000,
-                    }).show();
+                        $('.datatable').DataTable().ajax.reload();
 
-                },error: function (response) {
+                        new Noty({
+                            layout: 'topRight',
+                            type: 'alert',
+                            text: response,
+                            killer: true,
+                            timeout: 2000,
+                        }).show();
 
-                        data = response.responseJSON.message;
+                    },error: function (response) {
 
-                        swal({
-                            title: data + '😥',
-                            type: 'error',
-                            icon: 'error',
-                            buttons: false,
-                            timer: 15000
-                        }); //end of swal
+                            data = response.responseJSON.message;
 
-                    }, //end of error - success
+                            swal({
+                                title: data + '😥',
+                                type: 'error',
+                                icon: 'error',
+                                buttons: false,
+                                timer: 15000
+                            }); //end of swal
 
-            });//end of ajax call
+                        }, //end of error - success
 
-        });//end of delete
+                });//end of ajax call
 
-    }//end of if
+            });//end of delete
+
+        }//end of if
+
+        route = @json($datatables->sortable);
+
+        if(route) {
+
+            $("#data-table tbody").sortable({
+                items: 'tr',
+                cursor: 'move',
+                update: function(event, ui) {
+
+                    var sortedIDs = $(this).sortable('toArray', {attribute: 'data-id'});
+
+                    $.post(route, {order: sortedIDs}, (response) => {});
+                }
+            }).disableSelection();
+
+        }//end of check sortable
+
+    });//$(function()
 
 </script>
